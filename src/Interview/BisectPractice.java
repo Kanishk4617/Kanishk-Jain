d1200286468c00d04bf95f9f50722cdf84d80526
Kanishk.Jain <kanishk.jain@ril.com>
2023-06-25 15:25:16 +0530

    third commit

--- src/Interview/BisectPractice.java
+++ src/Interview/BisectPractice.java
@@ -3,6 +3,6 @@ package Interview;
 public class BisectPractice {
     public static void main(String[] args) {
         int c = 2+10;
-        System.out.println("sum =  + c);
+        System.out.println("sum ="  + c);
     }
 }
